1597188ef5b411cd49cb5debb3d79e5713be5a16
Yotam Salmon <yotam.salmon@gmail.com>
2026-07-05 11:49:49 +0300

    Build recipes

--- src/core/analysis_runner.py
+++ src/core/analysis_runner.py
@@ -1,5 +1,6 @@
 from .analysis import Analysis, AnalysisInput, AnalysisOutput
 from .artifacts import ArtifactManager
+from .recipe import Recipe
 
 
 class AnalysisExecutor(object):
@@ -14,3 +15,26 @@ class AnalysisExecutor(object):
         analysis_output = analysis.run(analysis_input)
 
         return analysis_output
+
+    def run_analysis_on_output(self, analysis: Analysis, previous_run: AnalysisOutput) -> AnalysisOutput:
+        input_dir = self.artifact_manager.get_analysis_output_directory(previous_run.analysis)
+        output_dir = self.artifact_manager.get_analysis_output_directory(analysis)
+
+        analysis_input = AnalysisInput(input_dir, output_dir, previous_run.analysis, previous_run)  # Do we need both output and analysis?
+        analysis_output = analysis.run(analysis_input)
+
+        return analysis_output
+
+    def run_recipe(self, recipe: Recipe, experiment_id: str, data_id: str) -> AnalysisOutput:
+        # TODO this should return something richer than only the last analysis' output
+
+        first_analysis, *rest_of_recipe = recipe.analyses
+
+        output = self.run_analysis_on_data(first_analysis, experiment_id, data_id)
+
+        for analysis in rest_of_recipe:
+            print("[+] Output is", output)
+            output = self.run_analysis_on_output(analysis, output)
+
+        print("[+] Final output is", output)
+        return output
